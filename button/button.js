d3d1775c17c59c160d77bbc18c66c082abaa08fc
894617385@qq.com <894617385@qq.com>
2018-11-08 18:03:21 +0800

    新增tab

--- button/button.js
+++ button/button.js
@@ -1,29 +1,35 @@
 Component({
   properties: {
+    // 内容文字
     text: {
       value: "",
       type: String
     },
+    // 传入背景色
     backgroundColor: {
       value: "",
       type: String
     },
+    // 传入字体颜色
     color: {
       value: "",
       type: String
     },
   },
   data: {
+    // 控制class的动态添加和减少
     show: false
   },
   methods: {
+    // 点击触发函数
     touchStart() {
       const that = this;
       that.setData({
         show: true
       })
     },
-    touchEnd(){
+    // 点击结束函数
+    touchEnd() {
       this.setData({
         show: false
       })
