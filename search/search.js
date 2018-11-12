8bb6827732b936966fe218e42a21682e23cce515
894617385@qq.com <894617385@qq.com>
2018-11-12 15:22:42 +0800

    修改搜索框

--- search/search.js
+++ search/search.js
@@ -1,54 +1,70 @@
+let timer
 Component({
   properties: {
-    // 字体图标颜色
-    color: {
-      type: String,
-      value: "#8c8c8c"
-    },
-    //字体图标大小
-    fontIcon: {
-      type: String,
-      value: "50rpx"
-    },
-    // 字体大小
-    font: {
-      type: String,
-      value: ""
-    },
-    // 选中反差颜色
-    focusColor: {
-      type: String,
-      value: "#bfbfbf"
-    },
-    // pleacehold
-    pleacehold: {
-      type: String,
-      value: "请输入单号"
-    }
+
   },
   data: {
-    blur: false
+    click: false,
+    focus: false,
+    value: ""
   },
   methods: {
-    focus() {
-      this.setData({
-        blur: true
-      })
-    },
-    blur() {
-      this.setData({
-        blur: false
-      })
-    },
-    //输入时间
-    bindinput: function (e) {
-      console.log(e.detail.value)
+    // 点击动画开始
+    click() {
+      clearTimeout(timer);
       const that = this;
-      const val = e.detail.value; //通过这个传递数据
-      const bindinput = {
+      that.setData({
+        click: true
+      })
+      timer = setTimeout(() => {
+        that.setData({
+          focus: true
+        })
+      }, 210)
+    },
+    // 失去焦点恢复动画
+    bindblur() {
+      const that = this;
+      if (this.data.value.length > 0) {
+        that.setData({
+          focus: false
+        })
+      } else {
+        that.setData({
+          click: false,
+          focus: false
+        })
+      }
+    },
+    //输入触发
+    input(e) {
+      this.setData({
+        value: e.detail.value
+      })
+      this.shutOut();
+    },
+    // 清空输入
+    clean() {
+      this.setData({
+        value: ""
+      })
+      this.shutOut();
+    },
+    // 取消
+    cancel() {
+      this.setData({
+        value: "",
+        click: false,
+        focus: false
+      });
+      this.shutOut();
+    },
+    // 发射函数
+    shutOut() {
+      const val = this.data.value
+      this.triggerEvent("input", {
         val: val
-      } // detail对象，提供给事件监听函数
-      this.triggerEvent('input', bindinput) //myevent自定义名称事件，父组件中使用
+      })
     }
   }
 })
