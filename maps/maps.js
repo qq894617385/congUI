baca40d0c36f799cef41d096f16ed5500656774a
unknown <qq894617385>
2018-11-29 18:17:45 +0800

    新增截图

--- maps/maps.js
+++ maps/maps.js
@@ -4,21 +4,23 @@ let qqmapsdk;
 Component({
   properties: {
     show: {
-      type: Boolean,
-      value: false
+      type: Boolean
     }
   },
   data: {
     // 地图指引
     latitude: 0, //地图初次加载时的纬度坐标
     longitude: 0, //地图初次加载时的经度坐标
-    name: "", //选择的位置名称,
+    name: "请选择地理位置", //选择的位置名称,
   },
   ready: function () {
+    const that = this;
     qqmapsdk = new QQMapWX({
       key: 'XFTBZ-AJX3V-H2IP4-UIXYS-3TZ7E-5LBKW'
     });
-    this.mapOpen();
+    if (this.properties.show) {
+      that.data.mapOpen()
+    }
   },
   methods: {
     mapOpen() {
@@ -30,11 +32,11 @@ Component({
       wx.chooseLocation({
         success: function (res) {
           console.log(res, "地理位置信息")
-          // res.name为地址名称  
+          // res.name为地址名称
           console.log(res.address);
-          //选择地点之后返回到原来页面
+          // 选择地点之后返回到原来页面
           that.setData({
-            DetailAddress: res.address,
+            name: res.address,
             mapShow: false,
             clickMaps: true
           })
